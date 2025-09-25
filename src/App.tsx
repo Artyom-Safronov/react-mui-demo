71a166633d16075cf6457abe094dbb12c89d6aa1
Artem Safronov <a.safronov@haulmont.com>
2025-09-25 11:12:54 +0400

    add template

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,155 @@
+
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Drawer,
+  IconButton,
+  Divider,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import { unstable_getScrollbarSize } from "@mui/utils";
+import { Menu } from "@mui/icons-material";
+type SidebarProps = CommonProps & {
+  toggleOpenDrawer: () => void;
+};
+
+type CommonProps = {
+  open: boolean;
+  openedDrawerWidth: number;
+  closedDrawerWidth: number;
+};
 function App() {
+  const [open, setOpen] = useState<false | true>(true);
+  const scrollBarSize = useTakeScrollWidth(open);
+  const openedDrawerWidth = 240 + scrollBarSize;
+  const closedDrawerWidth = 64 + scrollBarSize;
+  const toggleOpenDrawer = () => {
+    setOpen((prevState) => !prevState);
+  };
   return (
     <>
-      New React App
+      <Box sx={{ display: "flex" }}>
+        <Header {...{ open, openedDrawerWidth, closedDrawerWidth }} />
+        <Content {...{ open, openedDrawerWidth, closedDrawerWidth }} />
+        <Sidebar
+          {...{ open, openedDrawerWidth, closedDrawerWidth, toggleOpenDrawer }}
+        />
+      </Box>
     </>
-  )
+  );
 }
 
 export default App
+
+const useTakeScrollWidth = (open: boolean) => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const hasScroll =
+      document.documentElement.scrollHeight >
+      document.documentElement.clientHeight;
+
+    setWidth(hasScroll ? unstable_getScrollbarSize() : 0);
+  }, [open]);
+
+  return width;
+};
+
+function Sidebar({
+  open,
+  openedDrawerWidth,
+  closedDrawerWidth,
+  toggleOpenDrawer,
+}: SidebarProps) {
+  const scrollBarSize = useTakeScrollWidth(open);
+  return (
+    <Drawer
+      sx={(theme) => ({
+        width: open ? openedDrawerWidth : closedDrawerWidth,
+        flexShrink: 0,
+        transition: "width 225ms",
+        overflowX: "hidden",
+        "& .MuiDrawer-paper": {
+          width: open ? openedDrawerWidth : closedDrawerWidth,
+          boxSizing: "border-box",
+          transition: "width 225ms",
+          background: theme.palette.background.default,
+          "[aria-hidden] &": {
+            right: scrollBarSize,
+          },
+        },
+      })}
+      variant="permanent"
+      open={open}
+      anchor="right"
+    >
+      <Toolbar>
+        <IconButton
+          color="primary"
+          size="medium"
+          onClick={toggleOpenDrawer}
+          sx={(theme) => ({
+            position: "absolute",
+            left: theme.spacing(1.5),
+          })}
+        >
+          <Menu />
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      <Box
+        sx={(theme) => ({
+          my: theme.spacing(2.5),
+          py: 3,
+          px: 2,
+          overflowX: "hidden",
+        })}
+      ></Box>
+    </Drawer>
+  );
+}
+
+function Content({ open, openedDrawerWidth, closedDrawerWidth }: CommonProps) {
+  return (
+    <Box
+      component="main"
+      sx={{
+        width: open
+          ? `calc(100% - ${openedDrawerWidth}px)`
+          : `calc(100% - ${closedDrawerWidth}px)`,
+        flexGrow: 1,
+        bgcolor: "#fff",
+        padding: 2,
+        boxSizing: "border-box",
+        minHeight: "100vh",
+      }}
+    >
+      <Toolbar />
+      {<div>Content</div>}
+    </Box>
+  );
+}
+
+function Header({ open, openedDrawerWidth, closedDrawerWidth }: CommonProps) {
+  return (
+    <AppBar
+      position="fixed"
+      sx={(theme) => ({
+        width: open
+          ? `calc(100% - ${openedDrawerWidth}px)`
+          : `calc(100% - ${closedDrawerWidth}px)`,
+        marginRight: open ? `${openedDrawerWidth}px` : `${closedDrawerWidth}px`,
+        background: theme.palette.background.default,
+        boxShadow: "none",
+        border: "none",
+        borderBottom: `1px solid ${theme.palette.divider}`,
+        transition: "all 225ms",
+      })}
+    >
+      <Toolbar>
+        <img src="vite.svg" alt="" />
+      </Toolbar>
+    </AppBar>
+  );
+}
